958d5e7c956f1e68a810332cb6c206ad036539f6
Muhammadsandi0612 <sandimuhammad725@gmail.com>
2025-09-18 15:38:55 +0700

    Add files via upload

---
+++ Pertemuan 1/program test;.pas
@@ -0,0 +1,31 @@
+program test;
+
+uses crt;
+var 
+nama: string;
+umur: integer;
+
+jenis_kelamin : char;
+ip : real;
+
+const
+  pi = 3.14;
+  gravitasi =  9.8;
+
+begin
+  clrscr;
+  writeln('Nama Mahasiswa : '); readln(nama);
+  writeln('Umur Mahasiswa : '); readln(umur);
+  writeln('Jenis kelamin Mahasiswa : ');  readln(jenis_kelamin);
+  writeln('Ip mahasiswa : '); readln(ip);
+
+  clrscr;
+  writeln('Ini data yang anda masukkan'); 
+  writeln('nama Mahasiswa yang anda input : ', nama);  
+  writeln('Umur Mahasiswa yang anda input : ', umur);
+  writeln('Jenis kelamin Mahasiswa yang anda input :', jenis_kelamin);  
+  writeln('Ip mahasiswa yang anda input : ', ip);  
+
+
+end.
+
