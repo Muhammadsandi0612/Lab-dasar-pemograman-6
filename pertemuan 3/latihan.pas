a28a86eae938d15932edc4ac2a5abf224d99f4c6
Muhammadsandi0612 <sandimuhammad725@gmail.com>
2025-10-02 15:43:24 +0700

    Add files via upload

---
+++ pertemuan 3/latihan.pas
@@ -0,0 +1,18 @@
+uses crt;
+
+ var 
+  menu : integer;
+begin
+    clrscr;
+    write('masukkan Menu : '); readln(menu);
+    case menu of
+1: writeln('kopi');
+2: writeln('teh');
+3: writeln('jus');
+4: writeln('susu');
+5: writeln('air putih');
+ else
+  writeln('menu tidak tersedia');
+ end;
+
+ end.
